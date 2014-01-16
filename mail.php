2cf164da4b031ccc8f9e941ddc4a956614c6e9ab
Kevin Zhuang <kvzhuang@gmail.com>
2014-01-16 10:36:01 +0800

    Fix content.

--- mail.php
+++ mail.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 
-
-$path = "/home/miii/workspace/test-report"; 
+$path = "/home/miii/workspace/test-report";
 
 $latest_ctime = 0;
 
@@ -28,24 +27,27 @@ if (file_exists($file))
 {
     $xml = simplexml_load_string(file_get_contents($file));
 }
-foreach ($xml->children() as $testsuites) 
+//echo "<pre>"; print_r($xml);
+foreach ($xml->children() as $testsuites)
 {
     $error = intval($testsuites->attributes()->errors);
     $failures = intval($testsuites->attributes()->failures);
-    if (true|$error !=0 || $failures != 0 ) 
+    if (true|$error !=0 || $failures != 0 )
     {
-         $body = (string)$testsuites->attributes()->name ." has issues. please check\n";
+         $body = (string)$testsuites->attributes()->name ." Error:" . $error . ", Failures:" . $failures . ". \n";
          $body.= "http://devm1.corp.miiicasa.com/test-report/list.php?f=".$filename;
     }
 }
-if (true|$error != 0 || $failures != 0 ) 
+if (true|$error != 0 || $failures != 0 )
 {
+    
     $now = date("Y-m-d h:i:s");
     $from_name = "noreply@miiicasa.com";
     $from_email ="noreply@miiicasa.com";
-    $headers = "From testing";
+    $headers = "From:" . $from_email;
     $subject = $date["year"] . "/". $date["month"]."/".$date["day"]." ".$date["hour"].":00 - 測試報告";
-    $to = "kevin_zhuang@miiicasa.com";
+    $to = "kevin_zhuang@miiicasa.com,richard_wang@miiicasa.com,hunter_wu@miiicasa.com";
+    //$to = "kevin_zhuang@miiicasa.com";
     mail($to, $subject, $body, $headers);
 }
 
